5b72e85f713b04c492a25082309c0a7530129331
xuning <xuninghsu@gmail.com>
2017-03-26 10:37:20 +0800

    Init

---
+++ iOSURLSchemeDemo/iOSURLSchemeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSURLSchemeDemo
+//
+//  Created by xuning on 2017/3/26.
+//  Copyright © 2017年 xuning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
